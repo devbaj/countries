019f78d059904a23897de8073085e0de500f1d15
Brett Jory <bjory7@gmail.com>
2019-03-26 16:57:20 -0700

    queried cities in Mexico with pop > 500k, sorted descending

--- countries_assignment.sql
+++ countries_assignment.sql
@@ -7,4 +7,11 @@ JOIN languages ON countries.id = languages.country_id and languages.language = '
 SELECT countries.name, count(cities.id)
 FROM countries
 JOIN cities ON countries.id = cities.country_id
-GROUP BY countries.name;
+GROUP BY countries.name
+ORDER BY COUNT(cities.id) DESC;
+
+-- 3
+SELECT cities.name, cities.population
+FROM countries
+JOIN cities ON countries.id = cities.country_id AND countries.name = 'Mexico' AND cities.population < 500000
+ORDER BY cities.population DESC;
