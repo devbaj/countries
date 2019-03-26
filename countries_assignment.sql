3a04c27d2e1650ae01e478ec63f0cf47b50ac319
Brett Jory <bjory7@gmail.com>
2019-03-26 16:52:29 -0700

    queried number of cities in each country, sorted results by number of cities, descending

--- countries_assignment.sql
+++ countries_assignment.sql
@@ -1,3 +1,10 @@
+-- 1
 SELECT countries.name, languages.language, languages.percentage
 FROM countries
 JOIN languages ON countries.id = languages.country_id and languages.language = 'Slovene';
+
+-- 2
+SELECT countries.name, count(cities.id)
+FROM countries
+JOIN cities ON countries.id = cities.country_id
+GROUP BY countries.name;
